7bdb3e1745520f11f71cba9367ed9a80415e04db
Dylan <124385793@umail.ucc.ie>
2026-04-11 15:02:27 +0100

    Project Layout

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template
+import requests
+
+load_dotenv()
 
 app = Flask(__name__)
 
+api_key = os.getenv("DOG_API")
+url = "https://api.thedogapi.com/v1/images/search"
+
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
+    response = requests.get(url, headers={"x-api-key": api_key})
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+
+
+@app.route('/result')
+def result():
+    return 'Hello World!'
+
+@app.route('/health')
+def health():
+    return 'Hello World!'
+
+@app.route('/status')
+def status():
+    return 'Hello World!'
+
+@app.route('/logs')
+def logs():
     return 'Hello World!'
 
 
